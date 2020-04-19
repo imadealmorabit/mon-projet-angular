323a04cc14bf90153876c70c257ddf7eead75cfd
imadealmorabit <imadealmorabit@gmail.com>
2020-04-19 21:22:53 +0000

    routage avec parametre

--- src/app/services/appareil.service.ts
+++ src/app/services/appareil.service.ts
@@ -2,18 +2,29 @@ export class AppareilService {
 
   appareils = [
     {
+      id: 1,
       name: 'Machine à laver',
       status: 'allumé'
     },
     {
+      id: 2,
       name: 'Frigo',
       status: 'allumé',
     },
     {
+      id: 3,
       name: 'Ordinateur',
       status: 'éteint'
     }
   ];
+  getAppareilById(id: number){
+    const appareil = this.appareils.find(
+      (appareilObject) => {
+        return appareilObject.id === id;
+      }
+    );
+    return appareil;
+  }
 
 switchOnAll(){
   for (let appareil of this.appareils){
