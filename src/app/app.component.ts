d88e4993d9d96124b2a933e800d27bf3928abd65
imadealmorabit <imadealmorabit@gmail.com>
2020-04-21 13:05:11 +0000

    implementation d'un observable et le destroy pour eviter la boocle infini

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit} from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
 import {Observable, Subscription} from 'rxjs';
 import 'rxjs/Rx';
 
@@ -7,25 +7,23 @@ import 'rxjs/Rx';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent implements OnInit{
+export class AppComponent implements OnInit, OnDestroy{
 
   secondes: number;
+  counterSubscription: Subscription;
 
   constructor() {
   }
 
   ngOnInit(): void {
     const counter = Observable.interval(1000);
-    counter.subscribe(
-      (value: number) => {
-        this.secondes = value;
-      },
-      (error: any) => {
-        console.log('Une erreur a ete rencontree !')
-      },
-      () => {
-        console.log('Observable completee !');
-      }
-    );
+    this.counterSubscription = counter.subscribe(
+    (value: number) => {
+      this.secondes = value;
+    }
+  );
+}
+ngOnDestroy(): void {
+    this.counterSubscription.unsubscribe();
 }
 }
