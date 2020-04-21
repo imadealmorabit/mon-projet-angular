cf1947f68285474dfbea510c998ffa8438520343
imadealmorabit <imadealmorabit@gmail.com>
2020-04-21 12:58:59 +0000

    implementation d'un observable

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,31 @@
-import {Component} from '@angular/core';
+import { Component, OnInit} from '@angular/core';
+import {Observable, Subscription} from 'rxjs';
+import 'rxjs/Rx';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent{
+export class AppComponent implements OnInit{
+
+  secondes: number;
+
   constructor() {
   }
+
+  ngOnInit(): void {
+    const counter = Observable.interval(1000);
+    counter.subscribe(
+      (value: number) => {
+        this.secondes = value;
+      },
+      (error: any) => {
+        console.log('Une erreur a ete rencontree !')
+      },
+      () => {
+        console.log('Observable completee !');
+      }
+    );
+}
 }
