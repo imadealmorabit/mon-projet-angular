b482a581656f7ccbc66ffd03b55b2e72dda1dc8e
imadealmorabit <imadealmorabit@gmail.com>
2020-04-22 11:33:03 +0000

    implementation des subscriptions et des subjects

--- src/app/services/appareil.service.ts
+++ src/app/services/appareil.service.ts
@@ -1,6 +1,10 @@
+import {Subject} from 'rxjs';
+
 export class AppareilService {
 
-  appareils = [
+  appareilSubject = new Subject<any[]>();
+
+  private appareils = [
     {
       id: 1,
       name: 'Machine à laver',
@@ -17,6 +21,12 @@ export class AppareilService {
       status: 'éteint'
     }
   ];
+
+  emitAppareilSubject(){
+    this.appareilSubject.next(this.appareils.slice());
+  }
+
+
   getAppareilById(id: number){
     const appareil = this.appareils.find(
       (appareilObject) => {
@@ -27,21 +37,25 @@ export class AppareilService {
   }
 
 switchOnAll(){
-  for (let appareil of this.appareils){
+  for (const appareil of this.appareils){
     appareil.status = 'allumé';
   }
+  this.emitAppareilSubject();
 }
 switchOffAll(){
-  for (let appareil of this.appareils){
+  for (const appareil of this.appareils){
     appareil.status = 'éteint';
   }
+  this.emitAppareilSubject();
 }
   switchOnOne(index: number){
     this.appareils[index].status = 'allumé';
+    this.emitAppareilSubject();
   }
 
   switchOffOne(index: number){
     this.appareils[index].status = 'éteint';
+    this.emitAppareilSubject();
   }
 
 }
